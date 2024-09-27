3c67048da5fb9ec662814f5df77ef1c9916f6bfe
gydalo <gylo_l@hotmail.com>
2024-09-27 12:16:43 +0200

    fixed wrong pathing

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 // import "/fed2-js2-ca-gydalo/src/css/style.css";
 
-import router from "/fed2-js2-ca-gydalo/src/app.js";
+import router from "/fed2-js2-ca-gydalo/src/js/router/index.js";
 
 await router(window.location.pathname);
 
