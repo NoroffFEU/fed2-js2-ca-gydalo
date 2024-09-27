de883372e139f2711d38320320e5d5f37710e374
gydalo <gylo_l@hotmail.com>
2024-09-27 13:07:33 +0200

    Trying to fix pathing with a new approach

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -1,6 +1,55 @@
 // This function controls which JavaScript file is loaded on which page
 // In order to add additional pages, you will need to implement them below
 // You may change the behaviour or approach of this file if you choose
+
+export default async function router(pathname = window.location.pathname) {
+
+  console.log("Current pathname:", pathname);
+
+  const normalizedPath = pathname.replace(/\/$/, '');
+
+  console.log("Current pathname:", pathname);
+  console.log("Normalized pathname:", normalizedPath);
+
+  switch (normalizedPath) {
+    case "/fed2-js2-ca-gydalo/":
+      await import("/fed2-js2-ca-gydalo/src/js/router/views/home.js");
+      await import("/fed2-js2-ca-gydalo/src/js/router/views/buttons.js");
+      break;
+    case "/fed2-js2-ca-gydalo/index.html":
+      await import("/fed2-js2-ca-gydalo/src/js/router/views/home.js");
+      await import("/fed2-js2-ca-gydalo/src/js/router/views/buttons.js");
+      break;
+    case "/fed2-js2-ca-gydalo/auth":
+      await import("/fed2-js2-ca-gydalo/src/js/router/views/auth.js");
+      break;
+    case "/fed2-js2-ca-gydalo/auth/login":
+      await import("/fed2-js2-ca-gydalo/src/js/router/views/login.js");
+      break;
+    case "/fed2-js2-ca-gydalo/auth/register":
+      await import("/fed2-js2-ca-gydalo/src/js/router/views/register.js");
+      break;
+    case "/fed2-js2-ca-gydalo/post":
+      await import("/fed2-js2-ca-gydalo/src/js/router/views/post.js");
+      break;
+    case "/fed2-js2-ca-gydalo/post/edit":
+      await import("/fed2-js2-ca-gydalo/src/js/router/views/postEdit.js");
+      break;
+    case "/fed2-js2-ca-gydalo/post/create":
+      await import("/fed2-js2-ca-gydalo/src/js/router/views/postCreate.js");
+      break;
+    case "/fed2-js2-ca-gydalo/profile":
+      await import("/fed2-js2-ca-gydalo/src/js/router/views/profile.js");
+      break;
+    default:
+      await import("/fed2-js2-ca-gydalo/src/js/router/views/notFound.js");
+  }
+}
+
+
+
+
+/*
 export default async function router(pathname = window.location.pathname) {
 
   console.log("Current pathname:", pathname);
@@ -44,3 +93,5 @@ export default async function router(pathname = window.location.pathname) {
       await import("/fed2-js2-ca-gydalo/src/js/router/views/notFound.js");
   }
 }
+
+*/
