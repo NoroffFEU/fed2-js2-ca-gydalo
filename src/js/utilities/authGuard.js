f89c8281c18f38438a85fba1c04c6d288e1424af
gydalo <gylo_l@hotmail.com>
2024-09-27 10:59:56 +0200

    pathing

--- src/js/utilities/authGuard.js
+++ src/js/utilities/authGuard.js
@@ -1,6 +1,7 @@
 export function authGuard() {
   if (!localStorage.token) {
     alert("You must be logged in to view this page");
-    window.location.href = "/auth/login/";
+    window.location.href = "/fed2-js2-ca-gydalo/auth/login/index.html";
   }
 }
+
