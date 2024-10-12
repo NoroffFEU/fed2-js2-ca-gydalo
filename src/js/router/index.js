3d1675e42e80fd5f4c33373170d7ae1c0dab6e33
gydalo <gylo_l@hotmail.com>
2024-10-12 09:28:17 +0200

    Got rid of some console.logs I added for pathing issues

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -4,12 +4,7 @@
 
 export default async function router(pathname = window.location.pathname) {
 
-  console.log("Current pathname:", pathname);
-
   const normalizedPath = pathname.replace(/\/$/, '');
-
-  console.log("Current pathname:", pathname);
-  console.log("Normalized pathname:", normalizedPath);
 
   switch (normalizedPath) {
     case "/fed2-js2-ca-gydalo/":
@@ -67,52 +62,3 @@ export default async function router(pathname = window.location.pathname) {
   }
 }
 
-
-
-
-/*
-export default async function router(pathname = window.location.pathname) {
-
-  console.log("Current pathname:", pathname);
-  switch (pathname) {
-    case "/":
-      await import("/fed2-js2-ca-gydalo/src/js/router/views/home.js");
-      await import("/fed2-js2-ca-gydalo/src/js/router/views/buttons.js");
-      break;
-    case "/index.html":
-      await import("/fed2-js2-ca-gydalo/src/js/router/views/home.js");
-      await import("/fed2-js2-ca-gydalo/src/js/router/views/buttons.js");
-      break;
-    case "/auth":
-      await import("/fed2-js2-ca-gydalo/src/js/router/views/auth.js");
-      break;
-    case "/auth/login/":
-      await import("/fed2-js2-ca-gydalo/src/js/router/views/login.js");
-      break;
-    case "/auth/login/index.html":
-      await import("/fed2-js2-ca-gydalo/src/js/router/views/login.js");
-      break;
-    case "/auth/register/":
-      await import("/fed2-js2-ca-gydalo/src/js/router/views/register.js");
-      break;
-    case "/post/index.html":
-      await import("/fed2-js2-ca-gydalo/src/js/router/views/post.js");
-      break;
-    case "/edit/index.html":
-      await import("/fed2-js2-ca-gydalo/src/js/router/views/postEdit.js");
-      break;
-    case "/post/create/index.html":
-      await import("/fed2-js2-ca-gydalo/src/js/router/views/postCreate.js");
-      break;
-    case "/profile/index.html":
-      await import("/fed2-js2-ca-gydalo/src/js/router/views/profile.js");
-      break;
-      case "/profile/":
-        await import("/fed2-js2-ca-gydalo/src/js/router/views/profile.js");
-        break;
-    default:
-      await import("/fed2-js2-ca-gydalo/src/js/router/views/notFound.js");
-  }
-}
-
-*/
